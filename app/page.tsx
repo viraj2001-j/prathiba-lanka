638ca960a640f395378b4bdf0ab2c3b9836b96a1
Viraj <mviraj975@gmail.com>
2026-03-06 19:58:22 +0530

    footer remove

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function HomePage() {
         <ExperienceSection />
         <DetailsSection />
         <VisualStoriesSection />
-        <Footer />
+        
       </div>
     </main>
   );
